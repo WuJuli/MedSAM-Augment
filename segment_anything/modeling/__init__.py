7ccdf65f0ded2495b11df33cf7b61e7d7f0ceced
WuJuli <WRG_wh@163.com>
2023-09-25 18:18:02 +0800

    fpn

--- segment_anything/modeling/__init__.py
+++ segment_anything/modeling/__init__.py
@@ -15,11 +15,12 @@ from .image_encoder_ada import ImageEncoderViT  # add MultiScaleAdapter
 # from .image_encoder_vpt import ImageEncoderViT  # add vpt and adapter
 
 
-# from .mask_decoder import MaskDecoder      # my version hq mask decoder
+from .mask_decoder import MaskDecoder      # my version hq mask decoder
 
-from .mask_decoder_cascade import MaskDecoder  # not with hq, with cascade mask decoder
-from .mask_decoder_cascade import MaskDecoderHQ  # cascade mask decoder HQ
+# from .mask_decoder_cascade import MaskDecoder  # not with hq, with cascade mask decoder
+# from .mask_decoder_cascade import MaskDecoderHQ  # cascade mask decoder HQ
 
 # from .mask_decoder_ori import MaskDecoder  # the original decoder
 
 from .tiny_vit_sam import TinyViT
+from .fpn import FPN
